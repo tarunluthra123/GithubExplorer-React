dbdc18721b1aec7f947a4d2b9fcb3ec5c38ab9de
Tarun Luthra <tarunluthra987@gmail.com>
2020-07-12 17:43:51 +0530

    Fetched Repos and added load button

--- src/App.js
+++ src/App.js
@@ -1,60 +1,123 @@
 import React from 'react';
-import './App.css';
-import Search from "./components/search";
-// import Search2 from "./components/search2";
-// import Search3 from "./components/search3";
-import UserCard from "./components/UserCard";
+import Search from './components/search';
+import UserCard from './components/UserCard';
+import RepoCard from './components/RepoCard';
 
+const PAGE_SIZE = 10;
 
 class App extends React.Component {
     state = {
         user: null,
-        error: null,
-        loading: false
-    }
+        repos: [],
+        userDataError: null,
+        loading: false,
+        page: 1,
+    };
 
-    fetchUserData = async (username) => {
-        this.setState({
-            loading: true
-        }, async () => {
-            //Fetch Github API
+    fetchUserData = async username => {
+        const res = await fetch(`https://api.github.com/users/${username}`);
+        if (res.ok) {
+            const data = await res.json();
+
+            return {data};
+        }
+
+        const error = (await res.json()).message;
+
+        return {error};
+    };
+
+    fetchRepos = async username => {
+        const {page} = this.state;
+        const res = await fetch(
+            `https://api.github.com/users/${username}/repos?page=${page}&per_page=${PAGE_SIZE}`,
+        );
+        if (res.ok) {
+            const data = await res.json();
+
+            return {data, page: page + 1};
+        }
+
+        const error = (await res.json()).message;
+
+        return {error};
+    };
+
+    fetchData = async username => {
+        this.setState({loading: true}, async () => {
             try {
-                const res = await fetch('https://api.github.com/users/' + username)
-                if (res.ok) {
-                    const data = await res.json()
-                    // console.log(data)
+                const [user, repos] = await Promise.all([
+                    this.fetchUserData(username),
+                    this.fetchRepos(username),
+                ]);
+
+                if (user.data !== undefined && repos.data !== undefined) {
                     return this.setState({
-                        user: data,
-                        error: null,
-                        loading: false
-                    })
+                        user: user.data,
+                        repos: repos.data,
+                        page: repos.page,
+                        loading: false,
+                    });
                 }
 
-                const error = (await res.json()).message
-
                 this.setState({
-                    error: error,
-                    loading: false
-                })
-            } catch (e) {
-                console.log(e)
+                    userDataError: user.error,
+                    reposError: repos.error,
+                    loading: false,
+                });
+            } catch (err) {
                 this.setState({
-                    error: 'Some error',
-                    loading: false
-                })
+                    error: 'There was some error',
+                    loading: false,
+                });
             }
-        })
+        });
+    };
 
-    }
+    loadMore = async () => {
+        const {data, page} = await this.fetchRepos(this.state.user.login);
+
+        if (data)
+            this.setState(state => ({
+                repos: [...state.repos, ...data],
+                page,
+            }));
+    };
 
     render() {
-        const {error, loading, user} = this.state
+        const {
+            userDataError,
+            reposError,
+            loading,
+            user,
+            repos,
+            page,
+        } = this.state;
+
         return (
             <div>
-                <Search fetchData={this.fetchUserData}/>
-                {(loading && (<p>Loading...</p>))}
-                {error && <p className="text-danger">{error}</p>}
-                {!loading && !error && user && <UserCard user={this.state.user}/>}
+                <Search fetchData={this.fetchData}/>
+                <div className="container">
+                    <div className="text-center pt-5">
+                        {loading && <p>Loading...</p>}
+                        {userDataError && <p className="text-danger">{userDataError}</p>}
+                    </div>
+                    {!loading && !userDataError && user && <UserCard user={user}/>}
+                    {reposError && <p className="text-danger">{reposError}</p>}
+
+                    {!loading &&
+                    !reposError &&
+                    repos.map(repo => <RepoCard key={repo.id} repo={repo}/>)}
+
+                    {!loading &&
+                    !userDataError &&
+                    user &&
+                    (page - 1) * PAGE_SIZE < user.public_repos && (
+                        <button className="btn btn-success" onClick={this.loadMore}>
+                            Load More
+                        </button>
+                    )}
+                </div>
             </div>
         );
     }
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -6,7 +6,7 @@ class UserCard extends React.Component {
         return (
             <div className="card">
                 <div className="card-body">
-                    <img src={user.avatar_url}/>
+                    <img src={user.avatar_url} alt="User Avatar"/>
                     <h1>{user.name}</h1>
                     <p>{user.company}</p>
                     <p>{user.bio}</p>
