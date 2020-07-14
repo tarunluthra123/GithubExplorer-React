dcefd11a476db98cdbc81baed7380a04aae104f9
Tarun Luthra <tarunluthra987@gmail.com>
2020-07-14 12:18:30 +0530

    Updates

---
+++ precache-manifest.c3bc2cd21568953a70a6e0554c5582f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a216a3a2d46acd5a7e56d36359c6b6a",
+    "url": "/GithubExplorer-React/githubexplorer/index.html"
+  },
+  {
+    "revision": "4ce961be392565caaabb",
+    "url": "/GithubExplorer-React/githubexplorer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "094f71b69e92ed2c4ba3",
+    "url": "/GithubExplorer-React/githubexplorer/static/css/main.1fb3e76c.chunk.css"
+  },
+  {
+    "revision": "4ce961be392565caaabb",
+    "url": "/GithubExplorer-React/githubexplorer/static/js/2.764bb0e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GithubExplorer-React/githubexplorer/static/js/2.764bb0e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "094f71b69e92ed2c4ba3",
+    "url": "/GithubExplorer-React/githubexplorer/static/js/main.c55e96d9.chunk.js"
+  },
+  {
+    "revision": "55dd08ec7c95466b0ed1",
+    "url": "/GithubExplorer-React/githubexplorer/static/js/runtime-main.c0f8fd9c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/GithubExplorer-React/githubexplorer/precache-manifest.c3bc2cd21568953a70a6e0554c5582f3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GithubExplorer-React/githubexplorer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
